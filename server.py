87e8652bdb9f6556c9fb8b3e9ca461d3f1e0a679
conzty01 <conzty01@luther.edu>
2017-12-28 01:26:14 -0600

    Added functionality for displaying and filtering tags

--- server.py
+++ server.py
@@ -12,16 +12,66 @@ conn = psycopg2.connect(os.environ["DATABASE_URL"])
 @app.route("/")
 def index():
     c = conn.cursor()
-    c.execute("SELECT image, image_alt, title, date, abstract, id FROM posts ORDER BY id DESC;")
-    return render_template("index.html",posts=c.fetchall())
-
-@app.route("/post/<id>")
-def post(id):
-    c = conn.cursor()
-    c.execute("SELECT image, image_alt, title, date, body FROM posts WHERE id=%s;",(id))
     a = conn.cursor()
-    a.execute("SELECT id, image, image_alt, title, date FROM posts ORDER BY id DESC;")
-    return render_template("post.html",post=c.fetchone(),archive=a.fetchall())
+    t = conn.cursor()
+    c.execute("""
+    SELECT posts.image, posts.image_alt, posts.title, posts.date, posts.abstract, posts.id, tg
+    FROM posts JOIN
+        (SELECT post_tags.post_id, array_agg(tags.name) AS tg
+        FROM post_tags JOIN tags ON (post_tags.tag_id = tags.id)
+        GROUP BY post_tags.post_id) AS t ON posts.id = t.post_id
+    ORDER BY posts.date DESC;
+    """)
+
+    a.execute("SELECT id, image, image_alt, title, date FROM posts ORDER BY date DESC;")
+    t.execute("SELECT id, name FROM tags ORDER BY name ASC;")
+    return render_template("index.html",posts=c.fetchall(),archive=a.fetchall(),passiveTags=t.fetchall(),activeTags=())
+
+@app.route("/post/<pid>")
+def post(pid):
+    c = conn.cursor()
+    a = conn.cursor()
+    t = conn.cursor()
+    s = conn.cursor()
+    c.execute("""
+    SELECT posts.image, posts.image_alt, posts.title, posts.date, posts.body, posts.id, tg
+    FROM posts JOIN
+        (SELECT post_tags.post_id, array_agg(tags.name) AS tg
+        FROM post_tags JOIN tags ON (post_tags.tag_id = tags.id)
+        GROUP BY post_tags.post_id) AS t ON posts.id = t.post_id
+    WHERE id = %s;
+    """, (pid))
+
+    a.execute("SELECT id, image, image_alt, title, date FROM posts ORDER BY date DESC;")
+    t.execute("""SELECT tags.id, tags.name
+                 FROM tags JOIN post_tags ON (tags.id = post_tags.tag_id)
+                 WHERE post_tags.post_id != %s
+                 ORDER BY name ASC;""", (pid))
+    s.execute("""SELECT tags.id, tags.name
+                 FROM tags JOIN post_tags ON (tags.id = post_tags.tag_id)
+                 WHERE post_tags.post_id = %s
+                 ORDER BY name ASC;""", (pid))
+    return render_template("post.html",post=(c.fetchone(),),archive=a.fetchall(),passiveTags=t.fetchall(), activeTags=s.fetchall())
+
+@app.route("/tag/<tname>")
+def tags(tname):
+    c = conn.cursor()
+    a = conn.cursor()
+    t = conn.cursor()
+    s = conn.cursor()
+    c.execute("""
+    SELECT posts.image, posts.image_alt, posts.title, posts.date, posts.abstract, posts.id, tg
+    FROM posts JOIN
+        (SELECT post_tags.post_id, array_agg(tags.name) AS tg
+        FROM post_tags JOIN tags ON (post_tags.tag_id = tags.id)
+        GROUP BY post_tags.post_id) AS t ON posts.id = t.post_id
+    WHERE %s = ANY (tg);
+    """, (tname,))
+
+    a.execute("SELECT id, image, image_alt, title, date FROM posts ORDER BY date DESC;")
+    t.execute("SELECT * FROM tags WHERE name != %s ORDER BY name ASC;", (tname,))
+    s.execute("SELECT * FROM tags WHERE name = %s ORDER BY name ASC;", (tname,))
+    return render_template("post.html",post=c.fetchall(),archive=a.fetchall(),passiveTags=t.fetchall(), activeTags=s.fetchall())
 
 if __name__ == "__main__":
     app.run(debug=True)
